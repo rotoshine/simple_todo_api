f3428d895970cac737bc5c37a77ae5c7042bd271
rotoshine <rotoshine@gmail.com>
2019-06-25 18:30:34 +0900

    cors 처리

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 9090
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(cors())
+app.use(express.json())
 
 mongoose.Promise = global.Promise
 
@@ -18,13 +18,6 @@ mongoose
     console.log('Successfully connected to mongodb.')
   })
   .then(e => console.error(e))
-
-// cors
-app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  next()
-})
 
 app.use(require('./routes/todos'))
 
