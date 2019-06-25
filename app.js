e1367034d68ffac97730cb94f40bd9d62ad86713
rotoshine <rotoshine@gmail.com>
2019-06-25 17:56:26 +0900

    CORS 처리

--- app.js
+++ app.js
@@ -19,5 +19,11 @@ mongoose
   })
   .then(e => console.error(e))
 
-app.use('/', require('./routes/todos'))
+// cors
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
+})
+
 app.listen(port, () => console.log(`Server listening on port ${port}`))
